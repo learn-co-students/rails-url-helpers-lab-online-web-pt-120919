9105fb1bfbb59fc56c01096a3b50fb0d93a829ea
Christian_Fredericksen <christian.fredericksen@yahoo.com>
2020-04-27 17:36:00 -0700

    Done.

--- db/migrate/002_add_column_to_students.rb
+++ db/migrate/002_add_column_to_students.rb
@@ -1,4 +1,4 @@
-class AddActiveToStudents < ActiveRecord::Migration[5.0]
+class AddColumnToStudents < ActiveRecord::Migration[5.0]
   def change
     add_column :students, :active, :boolean, default: false
 
